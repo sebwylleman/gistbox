1c02d6f94893e5fc9b3d7ada7fe39678fea61aa7
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2023-12-05 06:45:19 +0000

    feat: new pages added

--- main.go
+++ main.go
@@ -6,12 +6,26 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	w.Write([]byte("Hello from gistbox"))
+}
+
+func gistView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific gist..."))
+}
+
+func gistCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new gist..."))
 }
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/gist/view", gistView)
+	mux.HandleFunc("/gist/create", gistCreate)
 
 	log.Print("starting server on: 4000")
 

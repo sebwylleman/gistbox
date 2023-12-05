4122d11b934c4715683b7588ca909a5891a3e3ee
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2023-12-05 06:11:52 +0000

    server and project setup refactored

--- main.go
+++ main.go
@@ -6,16 +6,15 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello from Gistbox"))
+	w.Write([]byte("Hello from gistbox"))
 }
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 
-	log.Print("starting server on :4000")
+	log.Print("starting server on: 4000")
 
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
-
 }
